b57929c255c2eef0d37d59937d9a5c34214b651b
bacchussafraz-boop <bacchus.safraz@gmail.com>
2026-01-09 01:36:11 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,16 @@
-const CACHE_NAME = 'acl-quest-v1';
-const ASSETS_TO_CACHE = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './manifest.json',
-    'https://cdn-icons-png.flaticon.com/512/3048/3048398.png'
+const CACHE_NAME = 'acl-quest-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
 ];
 
-// 1. Install Event: Saves files to the browser's storage
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Caching app assets...');
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// 2. Activate Event: Cleans up old versions of the app
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// 3. Fetch Event: Lets the app work offline
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
